f7bbb379f6a44b4c9c53543cdcf0c4f9ffcabe6f
Krishna Teja Dinavahi <krishnateja.dinavahi@zoominfo.com>
2020-06-07 21:23:15 -0400

    Added BoFA parser

--- gmail/api/api.py
+++ gmail/api/api.py
@@ -1,6 +1,7 @@
 import base64
 from gmail.api.redis_utils import RedisUtils
 from bs4 import BeautifulSoup
+import re
 
 
 class Api(RedisUtils):
@@ -14,7 +15,8 @@ class Api(RedisUtils):
             if card_query is not None:
                 results = self.service.users().messages().list(userId='me', q=card_query.decode('utf-8')).execute()
                 for message in results['messages']:
-                    message_content = self.service.users().messages().get(userId='me', id=message['id'], format='full').execute()
+                    message_content = self.service.users().messages().get(userId='me', id=message['id'],
+                                                                          format='full').execute()
                     email_content = ''
 
                     if 'data' in message_content['payload']['body'].keys():
@@ -24,19 +26,20 @@ class Api(RedisUtils):
                             if 'data' in part['body'].keys():
                                 email_content = part['body']['data'] + email_content
 
-                    test = bytes(str(email_content), encoding='utf-8')
-                    transaction_html = base64.urlsafe_b64decode(test).decode('utf-8')
+                    transaction_text = bytes(str(email_content), encoding='utf-8')
+                    transaction_html = base64.urlsafe_b64decode(transaction_text).decode('utf-8')
                     parser = self.get_parser(card)
-                    parser.parse(transaction_html)
 
+                    text_without_spaces = re.sub('\s+', ' ', transaction_html)
+                    parser.parse(text_without_spaces)
 
     def get_parser(self, card):
         if card == "Discover":
             return Discover()
         # if card == "Amex":
-            # return Amex()
-        # if card == "BoFA":
-            # return BoFA()
+        # return Amex()
+        if card == "BoFA":
+            return BoFA()
 
 
 class Discover:
@@ -66,6 +69,32 @@ class Discover:
 
         print(transaction_dict)
 
+
 # class Amex:
 
-# class BoFA:
+class BoFA:
+    def parse(self, transaction_html):
+        soup = BeautifulSoup(transaction_html, "html.parser")
+
+        amount_element = soup.find("td", text=" Amount: ")
+        transaction_dict = {}
+
+        if amount_element is not None:
+            transaction_amount = amount_element.find_parent().find_all('td')[-1].next_element.strip()
+            transaction_dict["amount"] = transaction_amount
+        else:
+            return
+
+        merchant_element = soup.find("td", text=" Merchant: ")
+
+        if merchant_element is not None:
+            transaction_merchant = merchant_element.find_parent().find_all('td')[-1].next_element.strip()
+            transaction_dict["merchant"] = transaction_merchant
+
+        date_element = soup.find("td", text=" Transaction date: ")
+
+        if date_element is not None:
+            transaction_date = date_element.find_parent().find_all('td')[-1].next_element.strip()
+            transaction_dict["date"] = transaction_date
+
+        print(transaction_dict)
